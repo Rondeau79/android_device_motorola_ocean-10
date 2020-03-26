cd80ed7c75b0d1309f767e73f7a763ce4cb47407
Rondeau79 <rondeau7@gmail.com>
2020-03-26 02:46:43 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/validus_ocean.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+COMMON_LUNCH_CHOICES= \
+    validus_ocean-eng \
+    validus_ocean-user \
+    validus_ocean-userdebug
