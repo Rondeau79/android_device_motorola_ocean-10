65aaa339fe4344c4c6f4e2d2d46e0fb3c4504da6
Rondeau79 <rondeau7@gmail.com>
2020-04-07 16:34:26 -0600

    Update and rename lineage_ocean.mk to cos_ocean.mk

--- cos_ocean.mk
+++ cos_ocean.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common CosmicOS stuff
-$(call inherit-product, vendor/cos/common.mk)
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit from ocean device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
