00db752f34445f28554cd868539fac9fdbaadd50
Dan Ford <56321359+grimtruth@users.noreply.github.com>
2020-04-22 01:07:43 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/xtended_ocean.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+    xtended_ocean-eng \
+    xtended_ocean-user \
+    xtended_ocean-userdebug
