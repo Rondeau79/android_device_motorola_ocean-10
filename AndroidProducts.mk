108c48fa54920d377750453daf490c3cd2c8bc7c
Rondeau79 <rondeau7@gmail.com>
2020-04-23 20:33:56 -0600

    Poison

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/scorpion_ocean.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+    scorpion_ocean-eng \
+    scorpion_ocean-user \
+    scorpion_ocean-userdebug
